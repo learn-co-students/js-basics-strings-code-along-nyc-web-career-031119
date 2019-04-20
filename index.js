9cee9da3cc90374e6009e78dec6406ac044a1d89
Theo Weller <weller.theodor@gmail.com>
2019-04-20 01:17:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const currentUser = 'Theo';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+function capitalize(string) {return string.toUpperCase()};
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + capitalize(currentUser) + '!';
+
+const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
 // Write your code in this file!
