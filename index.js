b1a43b19f9e128dded5b5d74958caee63b66a1aa
Yevgeniy <yevgeniyerot@gmail.com>
2019-04-21 15:15:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+let currentUser = 'What ever';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const firstInitial = currentUser[0];
+
+const shortGreeting = `Welcome, ${firstInitial}!`;
